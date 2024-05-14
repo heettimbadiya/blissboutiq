4e6623d9a52ff2354b92e4f7dc6a34475b6351a6
heettimbadiya <heettimbadiya650@gmail.com>
2024-05-14 16:55:53 +0530

    first commit

---
+++ src/components/global/theme/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {
+    // fontFamily: "Gontserrat",
+  },
+  palette: {
+    
+  },
+});
+export default theme;
