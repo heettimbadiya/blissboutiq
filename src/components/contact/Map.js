bc5d05b0461f6fab61ed4622f10616abda106e98
heettimbadiya <heettimbadiya650@gmail.com>
2024-05-23 18:32:20 +0530

    changge map

--- src/components/contact/Map.js
+++ src/components/contact/Map.js
@@ -37,7 +37,7 @@ const Map = () => {
           >
             <TileLayer
               attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
-              url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {/* <MarkerClusterGroup chunkedLoading> */}
             {markers.map((item, index) => (
